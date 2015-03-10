521283ab5fa55cac9fad2482bd6d563f855e0663
scroft001 <scroft001@gmail.com>
2015-03-10 14:57:35 -0700

    Monroe public library sign in
    Uses captcha type images to verify user

---
+++ Monroe public library 2/random.js
@@ -0,0 +1,16 @@
+/*
+   New Perspectives on HTML and CSS
+   Tutorial 10
+   Review Assignment
+
+
+   Function List:
+   randomInteger()
+      Used to reverse the order of characters in a text string
+
+*/
+
+
+function randomInteger(size) {
+   return Math.floor((size+1)*Math.random());
+}
